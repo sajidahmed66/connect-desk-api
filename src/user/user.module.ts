ba63f1964164249acc9ad652f84b6b037f35f854
forks007 <farhancuet014@gmail.com>
2023-08-19 21:10:42 +0600

    created the auth service

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,7 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth/auth.controller';
+import { AuthService } from './auth/auth.service';
 
 @Module({
-  controllers: [AuthController]
+  controllers: [AuthController],
+  providers: [AuthService]
 })
 export class UserModule {}

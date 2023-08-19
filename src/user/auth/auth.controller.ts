5a8d62ea0f41dcccc42754f8a50fb651efaeb3be
forks007 <farhancuet014@gmail.com>
2023-08-19 21:29:44 +0600

    creatd auth-dtos folder and set up register link

--- src/user/auth/auth.controller.ts
+++ src/user/auth/auth.controller.ts
@@ -1,4 +1,11 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Put } from '@nestjs/common';
+import { AuthService } from './auth.service';
 
 @Controller('auth')
-export class AuthController {}
+export class AuthController {
+  constructor(private readonly authService: AuthService) {}
+  @Post('/register')
+  signup() {
+    return this.authService.register();
+  }
+}

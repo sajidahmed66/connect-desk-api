fa128f89d4ea8c960345e00106bfb0655ed73ea4
forks007 <farhancuet014@gmail.com>
2023-08-21 03:20:08 +0600

    setup feature module with interceptor

--- src/features/features.module.ts
+++ src/features/features.module.ts
@@ -1,11 +1,18 @@
-import { Module } from '@nestjs/common';
+import { ClassSerializerInterceptor, Module } from '@nestjs/common';
 import { FeaturesController } from './features.controller';
 import { FeaturesService } from './features.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [PrismaModule],
   controllers: [FeaturesController],
-  providers: [FeaturesService],
+  providers: [
+    FeaturesService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ClassSerializerInterceptor,
+    },
+  ],
 })
 export class FeaturesModule {}

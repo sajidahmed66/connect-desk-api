12f153fe4c5a7b275e2789c2ae8ff3320c01efee
forks007 <farhancuet014@gmail.com>
2023-08-22 01:53:56 +0600

    grabbed the user from req in interceptor

--- src/user/interceptors/user.interceptor.ts
+++ src/user/interceptors/user.interceptor.ts
@@ -1,14 +1,12 @@
 import { CallHandler, ExecutionContext, NestInterceptor } from '@nestjs/common';
-import { Observable } from 'rxjs';
+import * as jwt from 'jsonwebtoken';
 
 export class UserInterceptor implements NestInterceptor {
-  intercept(
-    context: ExecutionContext,
-    next: CallHandler<any>,
-  ): Observable<any> | Promise<Observable<any>> {
+  async intercept(context: ExecutionContext, next: CallHandler<any>) {
     const request = context.switchToHttp().getRequest();
-    console.log({ request });
-
+    const token = request?.headers?.authorization?.split('Bearer ')[1];
+    const user = await jwt.decode(token);
+    request.user = user;
     return next.handle();
   }
 }

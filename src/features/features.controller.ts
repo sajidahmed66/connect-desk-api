1b23ff19a31bed716c98ff207c988304aabff8a7
forks007 <farhancuet014@gmail.com>
2023-08-22 15:00:16 +0600

    created guards and and authorized each end point

--- src/features/features.controller.ts
+++ src/features/features.controller.ts
@@ -7,6 +7,7 @@ import {
   ParseIntPipe,
   Post,
   Put,
+  UseGuards,
 } from '@nestjs/common';
 import { FeaturesService } from './features.service';
 import {
@@ -14,6 +15,9 @@ import {
   FeaturesResponseDto,
   UpdateFeatureDto,
 } from './dtos/features.dto';
+import { Roles } from 'src/decorators/roles.decorator';
+import { UserType } from '@prisma/client';
+import { AuthGuard } from 'src/guards/auth.guard';
 
 @Controller('features')
 export class FeaturesController {
@@ -24,11 +28,15 @@ export class FeaturesController {
     return this.featuresService.getFeatures();
   }
 
+  @Roles(UserType.ADMIN)
+  @UseGuards(AuthGuard)
   @Post('')
   createFeature(@Body() body: CreateFeatureDto) {
     return this.featuresService.createFeature(body);
   }
 
+  @Roles(UserType.ADMIN)
+  @UseGuards(AuthGuard)
   @Put(':id')
   async updateFeature(
     @Param('id', ParseIntPipe) id: number,
@@ -37,6 +45,8 @@ export class FeaturesController {
     return this.featuresService.updateFeature(id, body);
   }
 
+  @Roles(UserType.ADMIN)
+  @UseGuards(AuthGuard)
   @Delete(':id')
   async deleteFeature(@Param('id', ParseIntPipe) id: number) {
     return await this.featuresService.deleteFeature(id);

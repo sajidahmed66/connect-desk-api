fbe15279af401f127683da77191a20cc21e87eaf
forks007 <farhancuet014@gmail.com>
2023-09-01 19:15:03 +0600

    added interceptor in module

--- src/site-use-tutorial/site-use-tutorial.module.ts
+++ src/site-use-tutorial/site-use-tutorial.module.ts
@@ -1,11 +1,18 @@
-import { Module } from '@nestjs/common';
+import { ClassSerializerInterceptor, Module } from '@nestjs/common';
 import { SiteUseTutorialController } from './site-use-tutorial.controller';
 import { SiteUseTutorialService } from './site-use-tutorial.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [PrismaModule],
   controllers: [SiteUseTutorialController],
-  providers: [SiteUseTutorialService],
+  providers: [
+    SiteUseTutorialService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ClassSerializerInterceptor,
+    },
+  ],
 })
 export class SiteUseTutorialModule {}

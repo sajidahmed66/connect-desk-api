f4076e6324d1722b13fb8e22226d183e458d193c
forks007 <farhancuet014@gmail.com>
2023-09-01 22:49:28 +0600

    created post method

--- src/founder-message/founder-message.controller.ts
+++ src/founder-message/founder-message.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { FounderMessageService } from './founder-message.service';
+import { CreateFounderMessageDto } from './dtos/founder-message.dto';
 
 @Controller('founder-message')
-export class FounderMessageController {}
+export class FounderMessageController {
+  constructor(private readonly founderMessageService: FounderMessageService) {}
+
+  @Post('')
+  async createFounderMessage(@Body() body: CreateFounderMessageDto) {
+    return await this.founderMessageService.createFounderMessage(body);
+  }
+}
--- src/founder-message/founder-message.service.ts
+++ src/founder-message/founder-message.service.ts
@@ -1,7 +1,29 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 
+interface CreateFounderMessageParams {
+  name: string;
+  designation: string;
+  body: string;
+}
+
 @Injectable()
 export class FounderMessageService {
   constructor(private readonly prismaService: PrismaService) {}
+
+  async createFounderMessage({
+    name,
+    designation,
+    body,
+  }: CreateFounderMessageParams) {
+    const founderMessage = await this.prismaService.messageFromFounder.create({
+      data: {
+        name,
+        designation,
+        body,
+      },
+    });
+
+    return founderMessage;
+  }
 }

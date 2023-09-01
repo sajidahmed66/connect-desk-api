50bd35cf079879657e9c6e15c61a23579b775d5d
forks007 <farhancuet014@gmail.com>
2023-09-01 16:38:08 +0600

    added prisma module in contactus module

--- src/contact-us/contact-us.module.ts
+++ src/contact-us/contact-us.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ContactUsController } from './contact-us.controller';
 import { ContactUsService } from './contact-us.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [ContactUsController],
-  providers: [ContactUsService]
+  providers: [ContactUsService],
 })
 export class ContactUsModule {}

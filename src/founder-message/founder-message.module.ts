746786caf80e7fb43336c170bd4c688aac2b55d6
forks007 <farhancuet014@gmail.com>
2023-09-01 22:29:58 +0600

    added interceptor in module

--- src/founder-message/founder-message.module.ts
+++ src/founder-message/founder-message.module.ts
@@ -1,11 +1,18 @@
-import { Module } from '@nestjs/common';
+import { ClassSerializerInterceptor, Module } from '@nestjs/common';
 import { FounderMessageController } from './founder-message.controller';
 import { FounderMessageService } from './founder-message.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [PrismaModule],
   controllers: [FounderMessageController],
-  providers: [FounderMessageService],
+  providers: [
+    FounderMessageService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ClassSerializerInterceptor,
+    },
+  ],
 })
 export class FounderMessageModule {}

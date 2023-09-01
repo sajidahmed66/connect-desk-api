d9c546864c96e5974bb6f2907fc737b312336b3f
forks007 <farhancuet014@gmail.com>
2023-09-01 16:41:05 +0600

    added prisma service in contactus service

--- src/contact-us/contact-us.service.ts
+++ src/contact-us/contact-us.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class ContactUsService {}
+export class ContactUsService {
+  constructor(private readonly prismaService: PrismaService) {}
+}

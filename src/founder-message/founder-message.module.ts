624e7b462c91ae76617dadaf2da45ced97b0f5e5
forks007 <farhancuet014@gmail.com>
2023-09-01 22:27:44 +0600

    imported prismamodule in module

--- src/founder-message/founder-message.module.ts
+++ src/founder-message/founder-message.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { FounderMessageController } from './founder-message.controller';
 import { FounderMessageService } from './founder-message.service';
+import { PrismaModule } from 'src/prisma/prisma.module';
 
 @Module({
+  imports: [PrismaModule],
   controllers: [FounderMessageController],
-  providers: [FounderMessageService]
+  providers: [FounderMessageService],
 })
 export class FounderMessageModule {}

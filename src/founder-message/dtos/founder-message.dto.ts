0a09f1a91960f8241cd904d8a8fa8d87be491db5
forks007 <farhancuet014@gmail.com>
2023-09-01 23:00:51 +0600

    created responsedto

--- src/founder-message/dtos/founder-message.dto.ts
+++ src/founder-message/dtos/founder-message.dto.ts
@@ -1,4 +1,21 @@
+import { Exclude } from 'class-transformer';
 import { IsNotEmpty, IsString } from 'class-validator';
+
+export class FounderMessageResponseDto {
+  id: number;
+  name: string;
+  designation: string;
+  body: string;
+
+  @Exclude()
+  created_at: Date;
+  @Exclude()
+  updated_at: Date;
+
+  constructor(partial: Partial<FounderMessageResponseDto>) {
+    Object.assign(this, partial);
+  }
+}
 
 export class CreateFounderMessageDto {
   @IsString()

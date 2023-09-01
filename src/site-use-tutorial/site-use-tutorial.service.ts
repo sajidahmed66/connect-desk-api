c5e63a2f844c9340deab82bf593cbf18d4f24609
forks007 <farhancuet014@gmail.com>
2023-09-01 19:07:18 +0600

    added prisma service in service

--- src/site-use-tutorial/site-use-tutorial.service.ts
+++ src/site-use-tutorial/site-use-tutorial.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class SiteUseTutorialService {}
+export class SiteUseTutorialService {
+  constructor(private readonly prismaService: PrismaService) {}
+}

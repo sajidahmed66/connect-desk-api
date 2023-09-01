1accc78b60855ee14e56beec29d9e1d1cff04784
forks007 <farhancuet014@gmail.com>
2023-09-01 17:34:41 +0600

    setup contactus module with interceptor

--- src/contact-us/contact-us.module.ts
+++ src/contact-us/contact-us.module.ts
@@ -1,11 +1,18 @@
-import { Module } from '@nestjs/common';
+import { ClassSerializerInterceptor, Module } from '@nestjs/common';
 import { ContactUsController } from './contact-us.controller';
 import { ContactUsService } from './contact-us.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { APP_INTERCEPTOR } from '@nestjs/core';
 
 @Module({
   imports: [PrismaModule],
   controllers: [ContactUsController],
-  providers: [ContactUsService],
+  providers: [
+    ContactUsService,
+    {
+      provide: APP_INTERCEPTOR,
+      useClass: ClassSerializerInterceptor,
+    },
+  ],
 })
 export class ContactUsModule {}

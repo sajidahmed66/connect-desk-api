b2065e8d0e007ac8d5fb3d5398bcf1982cbe82f2
forks007 <farhancuet014@gmail.com>
2023-09-01 22:57:12 +0600

    added guard to post

--- src/founder-message/founder-message.controller.ts
+++ src/founder-message/founder-message.controller.ts
@@ -1,11 +1,14 @@
 import { Body, Controller, Post } from '@nestjs/common';
 import { FounderMessageService } from './founder-message.service';
 import { CreateFounderMessageDto } from './dtos/founder-message.dto';
+import { Roles } from 'src/decorators/roles.decorator';
+import { UserType } from '@prisma/client';
 
 @Controller('founder-message')
 export class FounderMessageController {
   constructor(private readonly founderMessageService: FounderMessageService) {}
 
+  @Roles(UserType.ADMIN)
   @Post('')
   async createFounderMessage(@Body() body: CreateFounderMessageDto) {
     return await this.founderMessageService.createFounderMessage(body);

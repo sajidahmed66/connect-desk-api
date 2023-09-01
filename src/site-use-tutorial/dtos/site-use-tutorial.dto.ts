78d6f86b78ba92bf28481350811550326b56374d
forks007 <farhancuet014@gmail.com>
2023-09-01 19:38:54 +0600

    created getresponsedto

--- src/site-use-tutorial/dtos/site-use-tutorial.dto.ts
+++ src/site-use-tutorial/dtos/site-use-tutorial.dto.ts
@@ -1,4 +1,27 @@
+import { Exclude, Expose } from 'class-transformer';
 import { IsNotEmpty, IsString, IsUrl } from 'class-validator';
+
+export class SiteUseTutorialResponseDto {
+  id: number;
+  title: string;
+
+  @Exclude()
+  video_link: string;
+  @Expose({ name: 'videoLink' })
+  videoLink() {
+    return this.video_link;
+  }
+
+  @Exclude()
+  created_at: Date;
+
+  @Exclude()
+  updated_at: Date;
+
+  constructor(partial: Partial<SiteUseTutorialResponseDto>) {
+    Object.assign(this, partial);
+  }
+}
 
 export class CreateSiteUseTutorialDto {
   @IsString()

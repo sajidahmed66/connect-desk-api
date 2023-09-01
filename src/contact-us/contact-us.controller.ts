6bd30f04207020db9927bba8a0a6957a31498ed9
forks007 <farhancuet014@gmail.com>
2023-09-01 18:44:35 +0600

    created  getContactus api

--- src/contact-us/contact-us.controller.ts
+++ src/contact-us/contact-us.controller.ts
@@ -1,10 +1,21 @@
-import { Body, Controller, Post } from '@nestjs/common';
-import { createContactUsDto } from './dtos/contact-us.dto';
+import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  ContactUsResponseDto,
+  createContactUsDto,
+} from './dtos/contact-us.dto';
 import { ContactUsService } from './contact-us.service';
+import { Roles } from 'src/decorators/roles.decorator';
+import { UserType } from '@prisma/client';
 
 @Controller('contact-us')
 export class ContactUsController {
   constructor(private readonly contactUsService: ContactUsService) {}
+
+  @Roles(UserType.ADMIN)
+  @Get('')
+  getContactUs(): Promise<ContactUsResponseDto[]> {
+    return this.contactUsService.getContactUs();
+  }
 
   @Post('')
   createContactUs(@Body() body: createContactUsDto) {
--- src/contact-us/contact-us.service.ts
+++ src/contact-us/contact-us.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
+import { ContactUsResponseDto } from './dtos/contact-us.dto';
 
 interface CreateContactUsParams {
   name: string;
@@ -12,6 +13,24 @@ interface CreateContactUsParams {
 export class ContactUsService {
   constructor(private readonly prismaService: PrismaService) {}
 
+  async getContactUs(): Promise<ContactUsResponseDto[]> {
+    const allContactUs = await this.prismaService.contactUs.findMany({
+      select: {
+        id: true,
+        name: true,
+        subject: true,
+        body: true,
+        created_at: true,
+      },
+    });
+
+    if (!allContactUs) {
+      throw new NotFoundException();
+    }
+
+    return allContactUs.map((data) => new ContactUsResponseDto(data));
+  }
+
   async createContactUs({ name, email, subject, body }: CreateContactUsParams) {
     const newContactUsMessage = await this.prismaService.contactUs.create({
       data: {
@@ -21,6 +40,6 @@ export class ContactUsService {
         body,
       },
     });
-    return newContactUsMessage;
+    return new ContactUsResponseDto(newContactUsMessage);
   }
 }

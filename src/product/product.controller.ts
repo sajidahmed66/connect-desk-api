3f5cde4d0f2f8403b6c86125de54fda202ad9919
sajidahmed66 <sajidahmedsiddiqui66@gmail.com>
2023-09-01 22:28:51 +0600

    product controller and module without testing

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -4,29 +4,50 @@ import {
   Delete,
   Get,
   Param,
+  ParseIntPipe,
   Post,
   Put,
 } from '@nestjs/common';
 import { ProductService } from './product.service';
-import { UpdateProductDto } from './dto/product.dto';
+import {
+  CreateProductDto,
+  ProductResponseDto,
+  UpdateProductDto,
+} from './dto/product.dto';
 
 @Controller('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
   @Get('')
-  getProducts() {}
+  getProducts(): Promise<ProductResponseDto[]> {
+    return this.productService.getAlProducts();
+  }
 
   @Get(':id')
-  getProductById() {}
+  getProductById(@Param('id', ParseIntPipe) id: number) {
+    return this.productService.getProductById(id);
+  }
 
   @Get('/image/:id')
-  getImageofProductById(@Param('id') id: number) {}
-  @Post('')
-  createProduct(@Body() body) {}
+  getImageofProductById(@Param('id', ParseIntPipe) id: number) {
+    return this.productService.getImageOfProduct(id);
+  }
 
-  @Put()
-  updateProduct(@Body() body: UpdateProductDto) {}
+  @Post('')
+  createProduct(@Body() body: CreateProductDto) {
+    return this.productService.createProduct(body);
+  }
+
+  @Put(':id')
+  updateProduct(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() body: UpdateProductDto,
+  ) {
+    return this.productService.updateProduct(id, body);
+  }
 
   @Delete(':id')
-  deleteProduct() {}
+  deleteProduct(@Param('id', ParseIntPipe) id: number) {
+    return this.productService.deleteProduct(id);
+  }
 }

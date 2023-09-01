6647c5011eb268205ee773fd7c023dd6ba904354
forks007 <farhancuet014@gmail.com>
2023-09-01 22:31:12 +0600

    added prismaservice in service

--- src/founder-message/founder-message.service.ts
+++ src/founder-message/founder-message.service.ts
@@ -1,4 +1,7 @@
 import { Injectable } from '@nestjs/common';
+import { PrismaService } from 'src/prisma/prisma.service';
 
 @Injectable()
-export class FounderMessageService {}
+export class FounderMessageService {
+  constructor(private readonly prismaService: PrismaService) {}
+}

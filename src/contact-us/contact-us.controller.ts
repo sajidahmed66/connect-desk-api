2ec081d89990203778056882bfec2a4480a1168e
forks007 <farhancuet014@gmail.com>
2023-09-01 18:04:46 +0600

    created post method for contactus

--- src/contact-us/contact-us.controller.ts
+++ src/contact-us/contact-us.controller.ts
@@ -1,4 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { createContactUsDto } from './dtos/contact-us.dto';
+import { ContactUsService } from './contact-us.service';
 
 @Controller('contact-us')
-export class ContactUsController {}
+export class ContactUsController {
+  constructor(private readonly contactUsService: ContactUsService) {}
+
+  @Post('')
+  createContactUs(@Body() body: createContactUsDto) {
+    return this.contactUsService.createContactUs(body);
+  }
+}
--- src/contact-us/contact-us.service.ts
+++ src/contact-us/contact-us.service.ts
@@ -1,7 +1,26 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from 'src/prisma/prisma.service';
 
+interface CreateContactUsParams {
+  name: string;
+  email: string;
+  subject: string;
+  body: string;
+}
+
 @Injectable()
 export class ContactUsService {
   constructor(private readonly prismaService: PrismaService) {}
+
+  async createContactUs({ name, email, subject, body }: CreateContactUsParams) {
+    const newContactUsMessage = await this.prismaService.contactUs.create({
+      data: {
+        name,
+        email,
+        subject,
+        body,
+      },
+    });
+    return newContactUsMessage;
+  }
 }
